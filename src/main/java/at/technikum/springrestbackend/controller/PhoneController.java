b7e602d2f42597fe766eace0811d67f3d15e3678
michi-1 <104499339+michi-1@users.noreply.github.com>
2024-01-16 22:15:22 +0100

    image upload for phones

--- src/main/java/at/technikum/springrestbackend/controller/PhoneController.java
+++ src/main/java/at/technikum/springrestbackend/controller/PhoneController.java
@@ -9,7 +9,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.UUID;
 
@@ -59,6 +64,36 @@ public class PhoneController {
     @PostMapping("/addPhone")
     public Phone createPhone(@RequestBody Phone phone) {
         return phoneService.createPhone(phone);
+    }
+    @PostMapping("/phones/upload/{id}")
+    public ResponseEntity<?> uploadImage(@PathVariable UUID id, @RequestParam("image") MultipartFile file) {
+         if (file.isEmpty()) {
+            return new ResponseEntity<>("Please select a file to upload.", HttpStatus.BAD_REQUEST);
+        }
+
+        try {
+             String directoryPath = "uploads"; // Replace with your directory path
+
+             Path directory = Paths.get(directoryPath);
+            if (!Files.exists(directory)) {
+                Files.createDirectories(directory);
+            }
+
+
+
+            String fileName = file.getOriginalFilename();
+            Path filePath = directory.resolve(fileName);
+
+            Files.copy(file.getInputStream(), filePath);
+            Phone phone = phoneService.getPhone(id) ;
+            phone.setPicture(filePath.toString());
+            return handlePhoneUpdate(id, phone);
+
+
+
+        } catch (IOException ex) {
+            return null;
+        }
     }
     @DeleteMapping("/deletePhone/{id}")
     public ResponseEntity<Object> deletePhone(@PathVariable UUID id) {
