775be099f5972675eac2b821b7f0e6372f79b752
michi-1 <104499339+michi-1@users.noreply.github.com>
2024-01-22 21:39:38 +0100

    Add files via upload

--- src/main/java/at/technikum/springrestbackend/service/PhoneService.java
+++ src/main/java/at/technikum/springrestbackend/service/PhoneService.java
@@ -1,20 +1,20 @@
 package at.technikum.springrestbackend.service;
 
+import at.technikum.springrestbackend.model.Brand;
 import at.technikum.springrestbackend.model.Phone;
+import at.technikum.springrestbackend.model.User;
 import at.technikum.springrestbackend.repository.PhoneRepository;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.UUID;
 
 @Service
+@AllArgsConstructor
 public class PhoneService {
 
     private final PhoneRepository phoneRepository;
-
-    public PhoneService(PhoneRepository phoneRepository) {
-        this.phoneRepository = phoneRepository;
-    }
 
     public List<Phone> getPhones() {
         return phoneRepository.findAll();
@@ -40,17 +40,29 @@ public class PhoneService {
         return phoneRepository.findByBattery(battery);
     }
 
+    public List<Phone> getPhoneByName(String name) {
+        return phoneRepository.findByName(name);
+    }
+
+
     public List<Phone> getPhonesPrice(float price) {
         return phoneRepository.findByPrice(price);
     }
 
-    public List<Phone> getPhonesBrand(String brand) {
+    public List<Phone> getPhonesBrand(Brand brand) {
         return phoneRepository.findByBrand(brand);
     }
 
     public Phone createPhone(Phone phone) {
         return phoneRepository.save(phone);
     }
-
+    @Transactional
+    public void deletePhone(UUID id) {
+        phoneRepository.deletePhoneById(id);
+    }
+    @Transactional
+    public int updatePhoneInfo(UUID oldId, String newName, String newDescription, float newDisplaySize, int newMemory, int newBattery, float newPrice, String newImage) {
+        return phoneRepository.updatePhoneInfo(oldId, newName, newDescription, newDisplaySize, newMemory, newBattery, newPrice, newImage);
+    }
 }
 
